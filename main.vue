3767a1b0ec99b3889749abf885fe8a012b9ef740
Kadeluxe <63540878+Kadeluxe@users.noreply.github.com>
2021-06-05 00:01:01 +0300

    Initial commit

---
+++ main.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  setup(props, ctx) {
+    return {};
+  },
+});
+</script>
+
+<style lang="scss">
+
+</style>
